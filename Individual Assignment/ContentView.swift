1275069c758e2cf59ecbf4eba17227c706824679
Cindy Chen <xchen@lcs.on.ca>
2023-11-08 21:07:51 -0500

    I have made the top red part

--- Individual Assignment/ContentView.swift
+++ Individual Assignment/ContentView.swift
@@ -10,12 +10,54 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Top part with red background
+            ZStack {
+                Rectangle()
+                        .frame(width: 400, height: 130)
+                        .foregroundColor(.red)
+                    .ignoresSafeArea()
+                
+               
+                HStack {
+                    VStack{
+                        HStack {
+                            Image(systemName: "gearshape")
+                                .foregroundColor(.white)
+                                .font(Font.system(size: 30))
+                                .padding()
+                            Spacer()
+                            
+                        }
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        
+                    }
+                    
+                    Text("Learn")
+                        .foregroundColor(.white)
+                        .font(Font.system(size: 25))
+                        .bold()
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                    
+                    Spacer(minLength: 165)
+                    
+                }
+                
+                   
+                    
+            }
+                
+            Spacer()
+            
+            
+            
+            
+            
         }
-        .padding()
+        
     }
 }
 
