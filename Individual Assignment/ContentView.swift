94ca08351eeb4385596e85cab89f73609da4ea92
Cindy Chen <xchen@lcs.on.ca>
2023-11-08 21:40:11 -0500

    I have added the image and the text for CPR. I have also customized and added the background color.

--- Individual Assignment/ContentView.swift
+++ Individual Assignment/ContentView.swift
@@ -9,58 +9,85 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            // Top part with red background
-            ZStack {
-                Rectangle()
-                        .frame(width: 400, height: 130)
-                        .foregroundColor(.red)
-                    .ignoresSafeArea()
+        
+        
+        
+        ZStack {
+            
+            Color.bggray
+                .ignoresSafeArea()
+            
+            VStack {
                 
-               
-                HStack {
-                    VStack{
-                        HStack {
-                            Image(systemName: "gearshape")
-                                .foregroundColor(.white)
-                                .font(Font.system(size: 30))
-                                .padding()
-                            Spacer()
+                // Top part with red background
+                ZStack {
+                    Rectangle()
+                            .frame(width: 400, height: 130)
+                            .foregroundColor(.red)
+                        .ignoresSafeArea()
+                    
+                   
+                    HStack {
+                        VStack{
+                            HStack {
+                                Image(systemName: "gearshape")
+                                    .foregroundColor(.white)
+                                    .font(Font.system(size: 30))
+                                    .padding()
+                                Spacer()
+                                
+                            }
+                            .padding(.bottom)
+                            .padding(.bottom)
+                            .padding(.bottom)
                             
                         }
-                        .padding(.bottom)
-                        .padding(.bottom)
-                        .padding(.bottom)
+                        
+                        Text("Learn")
+                            .foregroundColor(.white)
+                            .font(Font.system(size: 25))
+                            .bold()
+                            .padding(.bottom)
+                            .padding(.bottom)
+                            .padding(.bottom)
+                        
+                        Spacer(minLength: 165)
                         
                     }
                     
-                    Text("Learn")
-                        .foregroundColor(.white)
-                        .font(Font.system(size: 25))
-                        .bold()
-                        .padding(.bottom)
-                        .padding(.bottom)
-                        .padding(.bottom)
+                       
+                }
+                
+                
                     
-                    Spacer(minLength: 165)
+                Spacer()
+                
+                
+                VStack {
+                    Image("cprimage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 360)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    Text("Take a First Aid Course")
+                        .bold()
+                        .padding(.horizontal)
+                        .font(Font.system(size: 22))
+                        .multilineTextAlignment(.leading)
                     
                 }
                 
-                   
-                    
+                Spacer()
             }
-                
-            Spacer()
-            
-            
-            
-            
-            
         }
         
     }
+    
 }
+
 
 #Preview {
     ContentView()
+    
 }
